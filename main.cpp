3cfbf6f10f4617798fe54c9a9fcc0a3212083e5d
xstater <a166224@163.com>
2018-12-06 02:00:34 +0800

    添加了基本运行框架

--- main.cpp
+++ main.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+
+using namespace std;
 
 int main(int argc,char *argv[]){
-    std::cout<<"nmsl"<<std::endl;
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow(
+            "NMSL-Retro-Snaker",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            640,320,
+            SDL_WINDOW_SHOWN);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);//use hardware and enable the present vsync
+
+    bool is_quit = false;
+    SDL_Event event;
+    while(!is_quit){
+        SDL_SetRenderDrawColor(renderer,0,0,0,255);//set clear color to black
+        SDL_RenderClear(renderer);
+        while(SDL_PollEvent(&event)){
+            switch(event.type){
+                case SDL_WINDOWEVENT:
+                    if(event.window.event == SDL_WINDOWEVENT_CLOSE){//clicked red cross
+                        is_quit = true;
+                    }
+                    break;
+                case SDL_KEYDOWN:
+                    if(event.key.keysym.scancode == SDL_SCANCODE_UP){//up arrow
+                        cout<<"up"<<endl;
+                    }else if(event.key.keysym.scancode == SDL_SCANCODE_LEFT){//left arrow
+                        cout<<"left"<<endl;
+                    }else if(event.key.keysym.scancode == SDL_SCANCODE_RIGHT){//right arrow
+                        cout<<"right"<<endl;
+                    }else if(event.key.keysym.scancode == SDL_SCANCODE_DOWN){//down arrow
+                        cout<<"down"<<endl;
+                    }else if(event.key.keysym.scancode == SDL_SCANCODE_ESCAPE){//esc
+                        cout<<"esc"<<endl;
+                        is_quit = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        //draw sth here
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
     return 0;
 }
