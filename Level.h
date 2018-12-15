d311f3338cb2a5feb9c3809d84cfd9802033d290
xstater <a166224@163.com>
2018-12-15 22:27:04 +0800

    地图生成器

---
+++ Level.h
@@ -0,0 +1,16 @@
+#ifndef NMSL_RETRO_SNAKER_LEVEL_H
+#define NMSL_RETRO_SNAKER_LEVEL_H
+
+#include "Map.h"
+#include "Snake.h"
+
+class Level{
+public:
+    static void clear()noexcept;
+    static void genLevel1()noexcept;
+protected:
+private:
+};
+
+
+#endif //NMSL_RETRO_SNAKER_LEVEL_H
